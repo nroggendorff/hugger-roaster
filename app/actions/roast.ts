4f5469941597289cfb0a591e4735d36ab7c202eb
enzo <eesteves.dev@gmail.com>
2024-09-12 15:49:52 -0400

    use follow count through overview api

--- app/actions/roast.ts
+++ app/actions/roast.ts
@@ -1,7 +1,7 @@
-"use server"
+"use server";
 
 import { AutoTokenizer } from "@xenova/transformers";
-import { HfInference } from '@huggingface/inference'
+import { HfInference } from "@huggingface/inference";
 
 import { formatInformations, transformForInference } from "@/utils/roast";
 import { FormProps } from "@/components/form";
@@ -10,10 +10,15 @@ import prisma from "@/utils/prisma";
 const MODEL_ID = "meta-llama/Meta-Llama-3.1-70B-Instruct";
 
 export async function roast({ username, language }: FormProps) {
-  const userResponse = await fetch(`https://huggingface.co/api/users/${username}/overview`);
+  const userResponse = await fetch(
+    `https://huggingface.co/api/users/${username}/overview`
+  );
   const user = await userResponse.json();
   if (!user || user.error) {
-    return { error: user.error ?? "Something wrong happened, please retry.", status: 404 };
+    return {
+      error: user.error ?? "Something wrong happened, please retry.",
+      status: 404,
+    };
   }
 
   if (!username) {
@@ -21,63 +26,90 @@ export async function roast({ username, language }: FormProps) {
   }
 
   const requests = Promise.all([
-    await fetch(`https://huggingface.co/api/users/${username}/following`),
-    await fetch(`https://huggingface.co/api/users/${username}/followers`),
-    await fetch(`https://huggingface.co/api/spaces?author=${username}&sort=likes&limit=300&full=false&l`),
-    await fetch(`https://huggingface.co/api/models?author=${username}&sort=downloads&limit=300&full=false`),
-    await fetch(`https://huggingface.co/api/collections?owner=${username}&limit=100&sort=upvotes&full=false`)
+    await fetch(
+      `https://huggingface.co/api/spaces?author=${username}&sort=likes&limit=300&full=false&l`
+    ),
+    await fetch(
+      `https://huggingface.co/api/models?author=${username}&sort=downloads&limit=300&full=false`
+    ),
+    await fetch(
+      `https://huggingface.co/api/collections?owner=${username}&limit=100&sort=upvotes&full=false`
+    ),
   ]);
 
-  const [followingResponse, followersResponse, spacesResponse, modelsResponse, collectionsResponse] = await requests;
-  const [following, followers, spaces, models, collections] = await Promise.all([
-    followingResponse.json(),
-    followersResponse.json(),
+  const [spacesResponse, modelsResponse, collectionsResponse] = await requests;
+  const [spaces, models, collections] = await Promise.all([
     spacesResponse.json(),
     modelsResponse.json(),
-    collectionsResponse.json()
+    collectionsResponse.json(),
   ]);
-  const [spacesLikes, modelsLikes] = [spaces, models].map((items) => items.reduce((acc: number, item: any) => acc + item.likes, 0));
-  const collectionsUpvotes = collections?.reduce((acc: number, item: any) => acc + item.upvotes, 0);
-  
-  const datas = formatInformations(user, following.length, followers.length, spaces, models, collections,  spacesLikes, modelsLikes, collectionsUpvotes);
-  const chat = transformForInference(datas, language, user.fullname ?? username);
+  const [spacesLikes, modelsLikes] = [spaces, models].map((items) =>
+    items.reduce((acc: number, item: any) => acc + item.likes, 0)
+  );
+  const collectionsUpvotes = collections?.reduce(
+    (acc: number, item: any) => acc + item.upvotes,
+    0
+  );
+
+  const datas = formatInformations(
+    user,
+    spaces,
+    models,
+    collections,
+    spacesLikes,
+    modelsLikes,
+    collectionsUpvotes
+  );
+  const chat = transformForInference(
+    datas,
+    language,
+    user.fullname ?? username
+  );
 
   const hf = new HfInference(process.env.HF_ACCESS_TOKEN);
-  const tokenizer = await AutoTokenizer.from_pretrained("philschmid/meta-llama-3-tokenizer")
+  const tokenizer = await AutoTokenizer.from_pretrained(
+    "philschmid/meta-llama-3-tokenizer"
+  );
 
-  const formattedPrompt = tokenizer.apply_chat_template(chat, { tokenize: false, add_generation_prompt: true })
-  const res = await hf.textGeneration({
-    model: MODEL_ID,
-    inputs: formattedPrompt as string,
-    parameters: {
-      return_full_text: false,
-      max_new_tokens: 1024,
-      stop_sequences: ["<|end|>", "<|endoftext|>", "<|assistant|>"],
+  const formattedPrompt = tokenizer.apply_chat_template(chat, {
+    tokenize: false,
+    add_generation_prompt: true,
+  });
+  const res = await hf.textGeneration(
+    {
+      model: MODEL_ID,
+      inputs: formattedPrompt as string,
+      parameters: {
+        return_full_text: false,
+        max_new_tokens: 1024,
+        stop_sequences: ["<|end|>", "<|endoftext|>", "<|assistant|>"],
+      },
+    },
+    {
+      use_cache: false,
     }
-  }, {
-    use_cache: false,
-  })
+  );
 
   return {
-    data: res.generated_text
-  }
+    data: res.generated_text,
+  };
 }
 
-export async function getRoast({ id } : { id: string }) {
+export async function getRoast({ id }: { id: string }) {
   const roast = await prisma.quote.findUnique({
     where: {
-      id
-    }
-  })
+      id,
+    },
+  });
 
   if (!roast) {
     return {
       error: "Roast not found",
-      status: 404
-    }
+      status: 404,
+    };
   }
 
   return {
-    data: roast
-  }
+    data: roast,
+  };
 }
--- utils/roast.ts
+++ utils/roast.ts
@@ -2,8 +2,6 @@ import { Language } from "@/components/form";
 
 export const formatInformations = (
   user: any,
-  countFollowing: number,
-  countFollowers: number,
   spaces: any,
   models: any,
   collections: any,
@@ -12,70 +10,78 @@ export const formatInformations = (
   collectionsUpvotes: number
 ) => {
   const datas = {
-		name: user.fullname,
-		bio: user.details,
-		organizations: user.orgs?.map((org: any) => ({
-      name: org.fullname
+    name: user.fullname,
+    bio: user.details,
+    organizations: user.orgs?.map((org: any) => ({
+      name: org.fullname,
     })),
-		followers: countFollowers === 500 ? "500+" : countFollowers,
-		following: countFollowing === 500 ? "500+" : countFollowing,
+    followers: user.numFollowers,
+    following: user.numFollowers,
     total_spaces_likes: spacesLikes,
     total_models_likes: modelsLikes,
     total_collections_likes: collectionsUpvotes,
-		last_5_spaces: spaces
-			.map((space: any) => ({
-				name: space?.cardData?.title ?? space.id?.split("/")[1],
-				description: space?.cardData?.short_description,
-				likes_count: space.likes,
-				last_modified: space.lastModified,
-        created_at: space.createdAt
-			}))
-			.slice(0, 5),
+    last_5_spaces: spaces
+      .map((space: any) => ({
+        name: space?.cardData?.title ?? space.id?.split("/")[1],
+        description: space?.cardData?.short_description,
+        likes_count: space.likes,
+        last_modified: space.lastModified,
+        created_at: space.createdAt,
+      }))
+      .slice(0, 5),
     last_5_models: models
-    .map((model: any) => ({
-      name: model.id?.split("/")[1],
-      has_inference: model.inference,
-      likes_count: model.likes,
-      downloads_count: model.downloads,
-      pipeline_tag: model.pipeline_tag,
-      last_modified: model.lastModified,
-        created_at: model.createdAt
-    }))
-    .slice(0, 5),
+      .map((model: any) => ({
+        name: model.id?.split("/")[1],
+        has_inference: model.inference,
+        likes_count: model.likes,
+        downloads_count: model.downloads,
+        pipeline_tag: model.pipeline_tag,
+        last_modified: model.lastModified,
+        created_at: model.createdAt,
+      }))
+      .slice(0, 5),
     last_5_collections: collections
-    .map((collection: any) => ({
-      name: collection.title,
-      description: collection.description,
-      upvotes_count: collection.upvotes,
-    }))
-    .slice(0, 5),
-	};
+      .map((collection: any) => ({
+        name: collection.title,
+        description: collection.description,
+        upvotes_count: collection.upvotes,
+      }))
+      .slice(0, 5),
+  };
 
   return datas;
-}
+};
 
 export const transformForInference = (
   datas: Record<string, any>,
   language: Language,
-  username: string,
+  username: string
 ) => {
-  let user_content = `give a short and harsh roasting for the following hugging face profile: ${username}. Here are the details: "${JSON.stringify(datas)}"`
+  let user_content = `give a short and harsh roasting for the following hugging face profile: ${username}. Here are the details: "${JSON.stringify(
+    datas
+  )}"`;
 
   switch (language) {
     case "fr":
-      user_content = `fais une courte et cruelle critique sarcastique en argot pour le profil Hugging Face suivant en français :  ${username}. Voici les détails: "${JSON.stringify(datas)}"`
+      user_content = `fais une courte et cruelle critique sarcastique en argot pour le profil Hugging Face suivant en français :  ${username}. Voici les détails: "${JSON.stringify(
+        datas
+      )}"`;
       break;
     case "es":
-      user_content = `haz una crítica corta y cruel para el siguiente perfil de Hugging Face en español: ${username}. Aquí están los detalles: "${JSON.stringify(datas)}"`
+      user_content = `haz una crítica corta y cruel para el siguiente perfil de Hugging Face en español: ${username}. Aquí están los detalles: "${JSON.stringify(
+        datas
+      )}"`;
       break;
   }
 
-  const chat = [{
-    role: 'assistant',
-    content:
-      'You roast people hugging face account based on their bio, name, spaces, and models as harsh and spicy as possible, and keep it short.'
-  },
-  { role: 'user', content: user_content }]
+  const chat = [
+    {
+      role: "assistant",
+      content:
+        "You roast people hugging face account based on their bio, name, spaces, and models as harsh and spicy as possible, and keep it short.",
+    },
+    { role: "user", content: user_content },
+  ];
 
   return chat;
-}
+};
